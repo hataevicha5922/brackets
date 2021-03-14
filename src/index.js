f69783fa41dbd2b1b124e018d781c10dc4848996
hataevicha5922 <hataevicha5922@gmail.com>
2021-03-15 00:00:14 +0300

    feat: add result

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const parsedBracketsConfig = bracketsConfig.map((r) => {
+        return `${r[0]}${r[i]}`;
+    });
+    let prevLength = str.length;
+    while (str !== "") {
+        parsedBracketsConfig.forEach((element) => {
+            str = str.replaceAll(element, "");
+        });
+        if (prevLength === str.length) {
+            return false;
+        }
+        prevLength = str.length;
+    }
+    return true;
+};
